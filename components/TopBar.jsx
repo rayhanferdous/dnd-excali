4024e43a1abd6b57cb0d3416781fa883cde940fb
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2025-09-14 21:53:40 +0600

    fix hydration error

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -14,32 +14,30 @@ const TopBar = () => {
 
     return (<>
         <nav className="flex items-center justify-between">
-            <Button variant="secondary" size="icon">
-                <Drawer direction="left">
-                    <DrawerTrigger> <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                        />
-                    </svg></DrawerTrigger>
+            <Drawer direction="left">
+                <DrawerTrigger> <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                </svg></DrawerTrigger>
 
-                    <DrawerContent className={'px-12'} >
-                        <DrawerHeader>
-                            <DrawerTitle className={'text-seco'}>Fill Color</DrawerTitle>
-                            <DrawerDescription>Change color easily</DrawerDescription>
-                        </DrawerHeader>
-                        <ColorPicker />
-                    </DrawerContent>
-                </Drawer>
-            </Button>
+                <DrawerContent className={'px-12'} >
+                    <DrawerHeader>
+                        <DrawerTitle className={'text-seco'}>Fill Color</DrawerTitle>
+                        <DrawerDescription>Change color easily</DrawerDescription>
+                    </DrawerHeader>
+                    <ColorPicker />
+                </DrawerContent>
+            </Drawer>
             <ControlBox />
             <div className="flex gap-3.5 items-center">
                 <Button variant="default" size="sm">Share</Button>
